2af795307676811897056a7808f435f78213cf68
kingsley <kingsley@home-desktop>
2017-07-21 11:27:07 +0200

    base project setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# lantern
-lantern
---
+++ src/main/java/za/co/kmotsepe/lantern/BasicApplication.java
@@ -0,0 +1,20 @@
+package za.co.kmotsepe.lantern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Kingsley Motsepe
+ */
+@SpringBootApplication
+public class BasicApplication {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(BasicApplication.class, args);
+    }
+}
